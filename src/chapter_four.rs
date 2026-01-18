4ed16f8b3adb69606dc9ab82c03c34304dcd8849
kmfiguerrez <kmfiguerrez@gmail.com>
2026-01-18 08:33:16 +0800

    chore/add section.rs file.

--- src/chapter_four.rs
+++ src/chapter_four.rs
@@ -1,3 +1,4 @@
 pub mod four_point_one;
 pub mod four_point_two;
 pub mod four_point_three;
+pub mod section;
---
+++ src/chapter_four/section.rs
@@ -0,0 +1,5 @@
+use crate::chapter::Section;
+
+pub fn generate_sections() -> Vec<Section<'static>> {
+  vec![Section::new("What is ownership?")]
+}
