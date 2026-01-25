6fb2cafd7520a5cf72bfa8aad921e93c5d3cf3c2
kmfiguerrez <kmfiguerrez@gmail.com>
2026-01-25 01:52:36 +0000

    chore/make the content of seven_point_one.

--- src/chapter_seven/seven_point_one.rs
+++ src/chapter_seven/seven_point_one.rs
@@ -5,7 +5,7 @@ use owo_colors::OwoColorize;
 pub fn content(section_title: &str, section: &str) {
   let subheaders: [chapter::SubHeader; 1];
   subheaders = [
-    chapter::SubHeader::new("Bringing Paths into Scope with the use Keyword", bpiswuk_content),
+    chapter::SubHeader::new("Packages and Crates", pac_content),
 
   ];
 
@@ -14,31 +14,79 @@ pub fn content(section_title: &str, section: &str) {
 
 // Subheaders content below.
 
-// Subheader: Bringing Paths into Scope with the use Keyword. Abbreviated as bpiswuk.
-fn bpiswuk_content() {
-    menu::subheader_title("Bringing Paths into Scope with the use Keyword");
+// Subheader: Packages and Crates. Abbreviated as pac.
+fn pac_content() {
+    let solid_disc = "\u{2022}";
+    let two_spaces = "\u{2003}\u{2003}";
 
-    println!("\
-        Having to write out the paths to call functions can feel inconvenient and repetitive. \n\
-        Fortunately, there's a way to simplify this process: we can create a shortcut to a path with the {} keyword once, \n\
-        and then use the shorter name everywhere else in the scope. \n\n\
-        See: https://doc.rust-lang.org/book/ch07-04-bringing-paths-into-scope-with-the-use-keyword.html#bringing-paths-into-scope-with-the-use-keyword \n\
-        for code example and read the explanation until the next subheader: Creating Idiomatic use Paths, begins.
+    menu::subheader_title("Packages and Crates");
+
+    println!(
+      "A {0} is the smallest amount of code that the Rust compiler considers at a time. \n\
+      Even if you run {1} rather than {2} and pass a single source code file, the compiler considers that file to be a crate. \n\
+      Crates can contain modules, and the modules may be defined in other files that get compiled with the crate.
     ",
-    "use".green().bold()
+      "crate".italic(),
+      "rustc".bright_yellow().bold(),
+      "cargo".bright_yellow().bold(),
     );
 
-    println!("\
-        Adding {0} and a path in a scope is similar to creating a symbolic link in the filesystem. \n\
-        Paths brought into scope with {0} also check privacy, like any other paths.
-    ",  
-    "use".green().bold()
-    );
-
-    println!("\
-        Note that {0} only creates the shortcut for the particular scope (module scope) in which the {0} occurs. \n\
-        (A {0} statement only applies in the scope it's in.)
+    println!(
+      "A crate can come in one of two forms: \n\
+      {two_spaces}{solid_disc} a binary crate \n\
+      {two_spaces}{solid_disc} or a library crate. \n\n\
+      {0} are programs you can compile to an executable that you can run, such as a command line program or a server. \n\
+      Each must have a function called {1} that defines what happens when the executable runs. \n\n\
+      {2} don't have a {1} function, and they don't compile to an executable.\n\
+      Instead, they define functionality intended to be shared with multiple projects. \n\
+      Most of the time when Rustaceans say “crate,” they mean library crate, and they use “crate” interchangeably \
+      with the general programming concept of a “library.”
     ",
-    "use".green().bold()
+      "Binary crates".italic(),
+      "main".bright_yellow().bold(),
+      "Library crates".italic(),
     );
+
+    println!(
+      "The {0} root is a source file that the Rust compiler starts from and makes up the root module of your crate
+    ",
+      "crate".italic()
+    );
+
+    println!(
+      "A {0} is a bundle of one or more crates that provides a set of functionality. \n\
+      A package contains a {1} file that describes how to build those crates. \n\
+      Cargo is actually a package that contains the binary crate for the command line tool you've been using to build your code. \n\
+      The Cargo package also contains a library crate that the binary crate depends on. \n\
+      Other projects can depend on the Cargo library crate to use the same logic the Cargo command line tool uses. \n\n\
+      A package can contain as many binary crates as you like, but at most only one library crate. \n\
+      A package must contain at least one crate, whether that’s a library or binary crate.
+    ",
+      "package".italic(),
+      "Cargo.toml".italic(),
+    );
+
+    println!(
+      "Cargo follows a convention that {0} is the crate root of a binary crate with the same name as the package. \n\
+      Likewise, Cargo knows that if the package directory contains {1}, the package contains a library crate \
+      with the same name as the package, and {1} is its crate root. \n\
+      Cargo passes the crate root files to {2} to build the library or binary. \n\n\
+      A package can have multiple binary crates by placing files in the {3} directory: Each file will be a separate binary crate.
+    ",
+      "src/main.rs".italic(),
+      "src/lib.rs".italic(),
+      "rustc".bright_yellow().bold(),
+      "src/bin".italic(),
+    );
+
+    println!(
+      "{}\n\n\
+      {solid_disc} {1} is the crate root of a binary crate with the same name as the package \n\
+      {solid_disc} {2} is the crate root of a library crate with the same name as the package \n\
+      {solid_disc} In Rust, a file is considered a crate.
+    ",
+      "REMEMBER".bright_white().bold(),
+      "src/main.rs".italic(),
+      "src/lib.rs".italic(),
+    )
 }
