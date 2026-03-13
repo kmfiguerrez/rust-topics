57f5e3e6d08758ffe1b38b26690280b56d630381
kmfiguerrez <kmfiguerrez@gmail.com>
2026-03-13 10:03:38 +0000

    mend

--- src/chapter_eleven/eleven_point_one.rs
+++ src/chapter_eleven/eleven_point_one.rs
@@ -3,12 +3,15 @@ use crate::{chapter, menu};
 
 
 pub fn content(section_title: &str, section: &str) {
-  let subheaders: [chapter::SubHeader; 4];
+  let subheaders: [chapter::SubHeader; 7];
   subheaders = [
     chapter::SubHeader::new("Introduction", introduction_content),
     chapter::SubHeader::new("How to Write Tests", htwt_content),
     chapter::SubHeader::new("Structuring Test Functions", stf_content),
     chapter::SubHeader::new("Checking Results with assert!", crwa_content),
+    chapter::SubHeader::new("Testing Equality with assert_eq! and assert_ne!", tewegane_content),
+    chapter::SubHeader::new("Adding Custom Failure Messages", acfm_content),
+    chapter::SubHeader::new("Checking for Panics with should_panic", cfpws_content),
   ];
 
   chapter::SubHeader::prompt_subheader(&subheaders, section_title, section);
@@ -206,7 +209,8 @@ fn crwa_content() {
   menu::subheader_title("Checking Results with assert!");
 
   println!(
-    "The {0} macro, provided by the standard library, is useful when you want to ensure that some condition in a \
+    "This part talks about bug that can be caught with code testing.\n\n\
+    The {0} macro, provided by the standard library, is useful when you want to ensure that some condition in a \
     test evaluates to {1}.\n\
     We give the {0} macro an argument that evaluates to a Boolean.\n\
     If the value is {1}, nothing happens and the test passes.\n\
@@ -233,8 +237,218 @@ fn crwa_content() {
     "can_hold".bright_yellow().bold(),
     "assert!".bright_yellow().bold(),
     "Rectangle".bright_yellow().bold(),
-  )
+  );
+
+  println!(
+    "Note the use {0} line inside the {1} module.\n\
+    The {1} module is a regular module that follows the usual visibility rules: {2}\n\
+    Because the {1} module is an inner module, we need to bring the code under test (code under test means code being used \
+    in the test functions) in the outer module into the scope of the inner module.\n\
+    The glob (*) in the super means anything we define in the outer module is available to this tests module.
+  ",
+    "super::*;".bright_yellow().bold(),
+    "tests".bright_yellow().bold(),
+    "https://doc.rust-lang.org/book/ch07-03-paths-for-referring-to-an-item-in-the-module-tree.html".cyan()
+  );
+
+  println!(
+    "The rest of the code samples in the Checking Results with assert! subheader talks about different scenarios .i.e. \
+    what happens the smaller rectangle gets passed with larger rectangle and catching a bug when you flip the greater-than \
+    sign to > with the < less that sign.
+  ");
 
 }
 
+// Header: Testing Equality with assert_eq! and assert_ne!. Abbreviated as teweqane.
+fn tewegane_content() {
+  let solid_disc = "\u{2022}";
 
+  menu::subheader_title("Testing Equality with assert_eq! and assert_ne!");
+  
+  println!(
+    "A common way to verify functionality is to test for equality between the result of the code under test and \
+    the value you expect the code to return. \n\
+    You could do this by using the {0} macro and passing it an expression using the {1} operator.\n\
+    However, this is such a common test that the standard library provides a pair of macros—{2} and {3}—to perform \
+    this test more conveniently.\n\
+    These macros compare two arguments for equality or inequality, respectively.\n\
+    They'll also print the two values if the assertion fails, which makes it easier to see why the test failed; \
+    conversely, the {0} macro only indicates that it got a {4} value for the {1} expression, without printing \
+    the values that led to the {4} value.\n\n\
+    See Listing 11-7:{5}, for code sample
+  ",
+    "assert!".bright_yellow().bold(),
+    "==".bright_yellow().bold(),
+    "assert_eq!".bright_yellow().bold(),
+    "assert_ne!".bright_yellow().bold(),
+    "false".bright_yellow().bold(),
+    "https://doc.rust-lang.org/book/ch11-01-writing-tests.html#listing-11-7".cyan()
+  );
+
+  println!(
+    "In the part where the code is modified to introduce a bug, you'll see a message that the assertion \
+    that failed was {0} and what the {1} and {2} values are.\n\
+    This message helps us start debugging!\n\
+    You can imagine that this would be especially helpful when we have a lot of tests going on.
+  ",
+    "left == right".bright_yellow().bold(),
+    "left".bright_yellow().bold(),
+    "right".bright_yellow().bold(),
+  );
+
+  println!(
+    "{0}\n\n\
+    Note that in some languages and test frameworks, the parameters to equality assertion functions are called {1} and {2}, \
+    and the order in which we specify the arguments matters.\n\
+    However, in Rust, they're called {3} and {4}, and the order in which we specify the value we expect and the value the \
+    code produces doesn't matter.
+  ",
+    "TESTING IN OTHER LANGUAGE AND FRAMEWORKS".bright_magenta().bold(),
+    "expected".bright_yellow().bold(),
+    "actual".bright_yellow().bold(),
+    "left".bright_yellow().bold(),
+    "right".bright_yellow().bold(),
+  );
+
+  println!(
+    "The {0} macro will pass if the two values we give it are not equal and will fail if they are equal.\n\
+    {1}.\n\
+    For example, if we're testing a function that is guaranteed to change its input in some way, but the \
+    way in which the input is changed depends on the day of the week that we run our tests, the best thing \
+    to assert might be that the output of the function is not equal to the input.
+  ",
+    "assert_ne!".bright_yellow().bold(),
+    "This macro is most useful for cases when we're not sure what a value will be, but we know what the value \
+    definitely shouldn't be".bright_white().bold()
+  );
+
+  println!(
+    "Under the surface, the {0} and {1} macros use the operators {2} and {3}, respectively.\n\
+    When the assertions fail, these macros print their arguments using debug formatting, which \
+    means the values being compared must implement the {4} and {5} traits.\n\
+    All primitive types and most of the standard library types implement these traits.\n\
+    For structs and enums that you define yourself, you'll need to implement {4} to assert equality of those types.\n\
+    You'll also need to implement {5} to print the values when the assertion fails.\n\
+    Because both traits are derivable traits, as mentioned in Listing 5-12: {6} in Chapter 5, this is usually as straightforward \
+    as adding the {7} annotation to your struct or enum definition.
+  ",
+    "assert_eq!".bright_yellow().bold(),
+    "assert_ne!".bright_yellow().bold(),
+    "==".bright_yellow().bold(),
+    "!=".bright_yellow().bold(),
+    "PartialEq".bright_yellow().bold(),
+    "Debug".bright_yellow().bold(),
+    "https://doc.rust-lang.org/book/ch05-02-example-structs.html#listing-5-12".cyan(),
+    "#[derive(PartialEq, Debug)]".bright_yellow().bold(),
+  );
+
+  println!(
+    "{0}\n\n\
+    {solid_disc} In other languages and frameworks, the order of the arguments you pass to {1} and {2} matters. \
+    But in Rust the order of the arguments doesn't matter.\n\
+    {solid_disc} Under the surface, the {1} and {2} macros use the operators {3} and {4}, respectively, \
+    which means the values being compared must implement the {5} and {6} traits.\n\
+    {solid_disc} For structs and enums that you define yourself, you'll need to implement {5} to assert equality of those types.\n\
+    {solid_disc} You'll also need to implement {6} to print the values when the assertion fails.
+
+  ",
+    "REMEMBER".bright_white().bold(),
+    "assert_eq!".bright_yellow().bold(),
+    "assert_ne!".bright_yellow().bold(),
+    "==".bright_yellow().bold(),
+    "!=".bright_yellow().bold(),
+    "PartialEq".bright_yellow().bold(),
+    "Debug".bright_yellow().bold(),    
+  )
+}
+
+// Header: Adding Custom Failure Messages. Abbreviated as acfm.
+fn acfm_content() {
+  let solid_disc = "\u{2022}";
+
+  menu::subheader_title("Adding Custom Failure Messages");
+  
+  println!(
+    "You can also add a custom message to be printed with the failure message as optional arguments to the \
+    {0}, {1}, and {2} macros.\n\
+    Any arguments specified after the required arguments are passed along to the {3} macro \
+    (discussed in “Concatenating with + or format!”: {4} in Chapter 8), so you can pass a format string that contains {{}} \
+    placeholders and values to go in those placeholders.\n\
+    Custom messages are useful for documenting what an assertion means; when a test fails, you'll have a better idea of what \
+    the problem is with the code.\n\n\
+    See: {5}, for code samples and explanations.
+  ",
+    "assert!".bright_yellow().bold(),
+    "assert_eq!".bright_yellow().bold(),
+    "assert_ne!".bright_yellow().bold(),
+    "format!".bright_yellow().bold(),
+    "https://doc.rust-lang.org/book/ch08-02-strings.html#concatenating-with--or-format".cyan(),
+    "https://doc.rust-lang.org/book/ch11-01-writing-tests.html#adding-custom-failure-messages".cyan()
+  );
+
+  println!(
+    "{0}\n\n\
+    {solid_disc} Any arguments passed after the required arguments to assert macros are passed along to the {1} macro, \
+    meaning values can be be specified directly inside {{}}.
+  ",
+    "REMEMBER".bright_white().bold(),
+    "format!".bright_yellow().bold(),
+
+  )
+}
+
+// Header: Checking for Panics with should_panic. Abrreviated as cfpws.
+fn cfpws_content() {
+  menu::subheader_title("Checking for Panics with should_panic");
+
+  println!(
+    "In addition to checking return values, it's important to check that our code handles error conditions as we expect.\n\
+    For example, consider the Guess type that we created in Chapter 9, Listing 9-13: {0}.\n\
+    Other code that uses {1} depends on the guarantee that {1} instances will contain only values between 1 and 100.\n\
+    We can write a test that ensures that attempting to create a Guess instance with a value outside that range panics.\n\n\
+    We do this by adding the attribute {2} to our test function.\n\
+    The test passes if the code inside the function panics; the test fails if the code inside the function doesn't panic.\n\
+    Listing 11-8 shows a test that checks that the error conditions of Guess::new happen when we expect them to.\n\n\
+    Listing 11-8: {3}, for code sample.\n\n\
+    We place the {4} attribute after the {5} attribute and before the test function it applies to.
+  ",
+    "https://doc.rust-lang.org/book/ch09-03-to-panic-or-not-to-panic.html#listing-9-13".cyan(),
+    "Guess".bright_yellow().bold(),
+    "should_panic".bright_yellow().bold(),
+    "https://doc.rust-lang.org/book/ch11-01-writing-tests.html#listing-11-8".cyan(),
+    "#[should_panic]".bright_yellow().bold(),
+    "#[test]".bright_yellow().bold(),
+  );
+
+  println!(
+    "See: {0}, and look for the first codes that introduces bugs and explanations.\n\n\
+    Tests that use {1} can be imprecise.Test.\n\
+    A {1} test would pass even if the test panics for a different reason from the one we were expecting.\n\
+    To make {1} tests more precise, we can add an optional {2} parameter to the {1} attribute.\n\
+    The test harness will make sure that the failure message contains the provided text.\n\
+    For example, consider the modified code for {3} in Listing 11-9 where the {4} function panics with \
+    different messages depending on whether the value is too small or too large.\n\n\
+    Listing 11-9: {5}\n\n\
+    This test will pass because the value we put in the {1} attribute's expected parameter is a \
+    substring of the message that the {6} function panics with.\n\
+    We could have specified the entire panic message that we expect, which in this case would be {7}.\n\
+    What you choose to specify depends on how much of the panic message is unique or dynamic and how precise \
+    you want your test to be.\n\
+    In this case, a substring of the panic message is enough to ensure that the code in the test function executes \
+    the {8} case.\n\n\
+    The next reading introduces another bug.\n\
+    Again, see: {0}, for code sample and explanation in the bottom.
+  ",
+    "https://doc.rust-lang.org/book/ch11-01-writing-tests.html#checking-for-panics-with-should_panic".cyan(),
+    "should_panic".bright_yellow().bold(),
+    "expected".bright_yellow().bold(),
+    "Guess".bright_yellow().bold(),
+    "new".bright_yellow().bold(),
+    "https://doc.rust-lang.org/book/ch11-01-writing-tests.html#listing-11-9".cyan(),
+    "Guess::new".bright_yellow().bold(),
+    "Guess value must be less than or equal to 100, got 200".bright_yellow().bold(),
+    "else if value > 100".bright_yellow().bold(),
+  )
+}
+
+ 
